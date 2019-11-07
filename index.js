cc787e81e49705cfd623d9fa276803f881c3aa6d
henok welday <hwelday@mail.sfsu.edu>
2019-11-06 20:28:02 -0800

    lab 10

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 let visitCount = 0;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   visitCount++;
   const string = `Hello World! You have visited this app ${visitCount} times!`;
   console.log(string);
   res.send(string);
 });
 
-app.listen(3000);
+app.listen(3002);
